9b90bd086a5d341cee97b75ce313767b9c131fa4
SShrinivas95 <115466976+SShrinivas95@users.noreply.github.com>
2023-06-12 19:37:03 +0200

    Initial commit

---
+++ src/main/java/Projet/Tournoi/ProjetTournoiApplication.java
@@ -0,0 +1,13 @@
+package Projet.Tournoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetTournoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetTournoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Projet/Tournoi/ProjetTournoiApplicationTests.java
@@ -0,0 +1,13 @@
+package Projet.Tournoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetTournoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
